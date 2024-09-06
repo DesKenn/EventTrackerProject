e71fc57a7b6737219db80acad437e63986600b07
DesKenn <destineek423@gmail.com>
2024-09-06 16:02:35 -0600

    gradle dependencies

---
+++ YogaTracker/src/main/java/com/skilldistillery/yoga/YogaTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.yoga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YogaTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YogaTrackerApplication.class, args);
+	}
+
+}
